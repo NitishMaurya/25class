3e568956eebcac8da44ddde5d4ba9a42c2a57751
NitishMaurya <74291390+NitishMaurya@users.noreply.github.com>
2020-12-18 17:55:52 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,16 @@
+class Box extends BaseClass{
+//proterties
+
+constructor(x,y,width,height){
+
+  super(x,y,width,height);
+  this.image= loadImage("sprites/wood1.png");
+}
+
+}
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 25class
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
